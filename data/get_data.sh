603211838c0f822af1c6db33fac5afebaac21ea9
Johnson Li <johnsonli1993@163.com>
2016-07-01 09:28:54 +0800

    download data script

---
+++ data/get_data.sh
@@ -0,0 +1,6 @@
+
+wget http://prometheus-1151.appspot.com/log/http2_check_all -O http2_check.json
+
+wget http://prometheus-1151.appspot.com/log/all -O 'api_request_data.json'
+
+wget http://prometheus-1151.appspot.com/log/traffic_size_all -O 'traffic_size_all.json'
